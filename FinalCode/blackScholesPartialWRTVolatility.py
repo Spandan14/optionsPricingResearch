d9f4a9f7b145aed9fe60f219c8c48326627b619b
Spandan Goel <52139195+Spandan14@users.noreply.github.com>
2021-10-16 00:06:57 -0500

    Update blackScholesPartialWRTVolatility.py

--- FinalCode/blackScholesPartialWRTVolatility.py
+++ FinalCode/blackScholesPartialWRTVolatility.py
@@ -6,12 +6,10 @@ def blackScholesPartialWRTVolatility(tau, r, S_0, K, v, q, callOrPut):
     NPrime = norm.pdf
     mult = 2 * (callOrPut.value - 1.5)
 
-    d1 = mult * ((np.log(S_0 / K) + tau * ((v ** 2) / 2 + r)) / (v * math.sqrt(tau)))
-    d2 = mult * (d1 - v * math.sqrt(tau))
+    d1 = (np.log(S_0 / K) + tau * ((v ** 2) / 2 + r)) / (v * math.sqrt(tau)))
+    d2 = d1 - v * math.sqrt(tau)
     
-    d1Partial = math.sqrt(tau) - (tau * ((v ** 2) / 2 + r) + np.log(S_0 / K)) / (v ** 2 * math.sqrt(tau))
-    d2Partial = d1Partial - math.sqrt(tau)
+    d1Partial = mult * (math.sqrt(tau) - (tau * ((v ** 2) / 2 + r) + np.log(S_0 / K)) / (v ** 2 * math.sqrt(tau)))
+    d2Partial = mult * (d1Partial - math.sqrt(tau))
 
-    return mult * (S_0 * math.e ** (-q * tau) * NPrime(d1) * d1Partial - K * math.e ** (-r * tau) * NPrime(d2) * d2Partial)
-
-
+    return mult * (S_0 * math.e ** (-q * tau) * NPrime(mult * d1) * d1Partial - K * math.e ** (-r * tau) * NPrime(mult * d2) * d2Partial)
